3fc043401972162ef478ce376c620d7813a946fa
Marcio Ferreira <Marcio.Ferreira@fit-tecnologia.org.br>
2026-05-26 15:57:55 -0400

    ok

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import uvicorn
 
 if __name__ == "__main__":
+    print("Servidor rodando em http://127.0.0.1:8000  |  Ctrl+C para parar")
     uvicorn.run(
         "main:app",
         host="127.0.0.1",
